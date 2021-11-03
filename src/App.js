2fe10e5d7550036e697bce0c862b71f62252851c
Syed Shah <moazzam.shah106@gmail.com>
2021-11-03 14:56:26 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
+// import Test from './components/Test';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      monsters: [],
+    };
+  }
+  componentDidMount() {
+    axios
+      .get('https://jsonplaceholder.typicode.com/users')
+      .then((res) => this.setState({ monsters: res.data }));
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        {this.state.monsters.map((user) => (
+          <h2 key={user.id}> {user.name} </h2>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
