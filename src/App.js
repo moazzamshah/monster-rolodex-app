b6da2d0d31d35e3bbb5bd2b5a94df0a3b483fb2e
Syed Shah <moazzam.shah106@gmail.com>
2021-11-04 02:48:51 +0100

    added the search functionality in page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
     super(props);
     this.state = {
       monsters: [],
+      searchField: '',
     };
   }
   componentDidMount() {
@@ -18,10 +19,21 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLocaleLowerCase())
+    );
     return (
       <div className='App'>
+        <input
+          type='search'
+          placeholder='Search monsters'
+          onChange={(e) => {
+            this.setState({ searchField: e.target.value });
+          }}
+        />
         <div className='card-list'>
-          <CardList monsters={this.state.monsters} />
+          <CardList monsters={filteredMonsters} />
         </div>
       </div>
     );
